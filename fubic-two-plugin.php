b0b7c27d9cb9d2ed1df67aee226a2ea049579aa4
Sulaiman Misri <msulaimanmisri@gmail.com>
2023-04-11 11:41:29 +0800

    Add version 1.3

--- fubic-two-plugin.php
+++ fubic-two-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Fubic Two
  * Plugin URI:        https://github.com/Asia-Quest-Malaysia
  * Description:       Compilation of Fubic requirement (Task two) in one single plugin.
- * Version:           1.2
+ * Version:           1.3
  * Author:            Sulaiman Misri
  * Author URI:        https://github.com/msulaimanmisri
  * Text Domain:       sm-fubic-two
@@ -20,7 +20,24 @@ require 'classes/enqueueController.php';
 
 /**
  * Controller Files
+ * The plugin need a Actived WooCommerce Plugin.
  */
-require 'classes/woocommerceMyAccountController.php';
-require 'classes/pageController.php';
-require 'classes/shortcodeController.php';
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    require 'classes/woocommerceMyAccountController.php';
+    require 'classes/pageController.php';
+    require 'classes/shortcodeController.php';
+}
+
+/**
+ * If WooCommerce is not active, then execute the handling dependency.
+ */
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    add_action('admin_notices', 'fubicTwoNeedWooCommerceToBeActivated');
+}
+
+function fubicTwoNeedWooCommerceToBeActivated()
+{
+    $class = 'notice notice-error';
+    $message = __('Fubic Two : You need to activate the WooCommerce plugin first in order to make this plugin work properly.', 'textdomain');
+    printf('<div class="%1$s"><p><b>%2$s</b></p></div>', esc_attr($class), esc_html($message));
+}
